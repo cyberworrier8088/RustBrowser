f1f9c039a24f8470ad71951cef09408195b01cf3
cyberworrier8088 <cyberworrier39@gmail.com>
2026-06-13 17:07:06 +0530

    update: img connot load in internet images fixed but current some probms and lagy

--- src/net.rs
+++ src/net.rs
@@ -6,3 +6,12 @@ pub fn fetch_page(url: &str) -> Result<String, String> {
         .text()
         .map_err(|error| error.to_string())
 }
+
+pub fn fetch_image(url: &str) -> Result<Vec<u8>, String> {
+    reqwest::blocking::get(url)
+        .map_err(|error| error.to_string())?
+        .bytes()
+        .map(|bytes| bytes.to_vec())
+        .map_err(|error| error.to_string())
+}
+
